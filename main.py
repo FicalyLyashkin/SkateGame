f74d83807563f79c9eb56af825019bb856b8f43a
elizashanker <122374450+elizashanker@users.noreply.github.com>
2023-12-30 19:28:37 +0300

    Update main.py
    соединила все в один файл, сделала трубы рабочими, убрала баг с пропаданием игрока

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import random
 import sys
-from start import *
-from end import *
+import os
+import pygame
+
+WIDTH = 1300
+HEIGHT = 900
+FPS = 60
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -84,6 +104,144 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
             self.speedy = 0
 
+sprites_names = {
+    'ramp': Ramp,
+    'cone': Cone,
+    'obstracle': Obstacle
+}
+
+tile_width = tile_height = 50
+
+
+
+def generate_level(level):
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                pass
+            elif level[y][x] == '*':
+                lane_y = lanes_y[y]
+                cone = Cone(lane_y - 50)
+                all_sprites.add(cone)
+                cones.add(cone)
+            elif level[y][x] == '-':
+                lane_y = lanes_y[y]
+                obstacle = Obstacle(lane_y - 50)
+                all_sprites.add(obstacle)
+                obstacles.add(obstacle)
+            elif level[y][x] == "+":
+                lane_y = lanes_y[y]
+                ramp = Ramp(lane_y)
+                all_sprites.add(ramp)
+                ramps.add(ramp)
+
+    return x, y
+
+
+pygame.init()
+clock = pygame.time.Clock()
+
+
+class Start:
+    def __init__(self):
+        self.light = False
+
+    def start_screen(self):
+        intro_text = ["НАЗВАНИЕ ИГРЫ", "",
+                      "Правила игры"]
+
+        fon = pygame.transform.scale(load_image('city.png'), (WIDTH, HEIGHT))
+        screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 50)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('black'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 1
+            intro_rect.top = text_coord
+            intro_rect.x = 50
+            text_coord += intro_rect.height
+            screen.blit(string_rendered, intro_rect)
+        pygame.draw.rect(screen, pygame.Color(153, 192, 212), (WIDTH // 3, HEIGHT // 6 * 4, WIDTH // 3, HEIGHT // 6), 0)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] in range(WIDTH // 3, WIDTH // 3 * 2) and \
+                        event.pos[1] in range(HEIGHT // 6 * 4, HEIGHT // 6 * 5):
+                    return  # начинаем игру
+                elif event.type == pygame.MOUSEMOTION:
+                    if event.pos[0] in range(WIDTH // 3, WIDTH // 3 * 2) and event.pos[1] in range(HEIGHT // 6 * 4,
+                                                                                                   HEIGHT // 6 * 5):
+                        self.light = True
+                    else:
+                        self.light = False
+
+            if self.light:
+                pygame.draw.rect(screen, pygame.Color(210, 228, 236),
+                                 (WIDTH // 3, HEIGHT // 6 * 4, WIDTH // 3, HEIGHT // 6), 0)
+            else:
+                pygame.draw.rect(screen, pygame.Color(153, 192, 212),
+                                 (WIDTH // 3, HEIGHT // 6 * 4, WIDTH // 3, HEIGHT // 6), 0)
+            pygame.display.flip()
+            clock.tick(FPS)
+
+    def terminate(self):
+        pygame.quit()
+        sys.exit()
+
+
+class End:
+    def __init__(self):
+        self.on_btn = False
+
+    def end_screen(self):
+        intro_text = ["ВЫ ПРОИГРАЛИ", "",
+                      "Счёт: {}".format(count_points // 5)]
+
+        screen.fill(pygame.Color(153, 192, 212))
+        font = pygame.font.Font(None, 70)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('black'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 1
+            intro_rect.top = text_coord
+            intro_rect.x = WIDTH // 3
+            text_coord += intro_rect.height
+            screen.blit(string_rendered, intro_rect)
+        btn_coords = (WIDTH // 3, HEIGHT // 6)
+        pygame.draw.rect(screen, pygame.Color(153, 192, 212), (btn_coords[0], btn_coords[1] * 4, *btn_coords), 0)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] in range(btn_coords[0],
+                                                                                    btn_coords[0] * 2) and \
+                        event.pos[1] in range(btn_coords[1] * 4, btn_coords[1] * 5):
+                    self.terminate()
+                elif event.type == pygame.MOUSEMOTION:
+                    if (event.pos[0] in range(btn_coords[0], btn_coords[0] * 2) and
+                            event.pos[1] in range(btn_coords[1] * 4, btn_coords[1] * 5)):
+                        self.on_btn = True
+                    else:
+                        self.on_btn = False
+
+            if self.on_btn:
+                pygame.draw.rect(screen, pygame.Color(50, 50, 50),
+                                 (WIDTH // 3, HEIGHT // 6 * 4, WIDTH // 3, HEIGHT // 6), 0)
+            else:
+                pygame.draw.rect(screen, pygame.Color(0, 0, 0),
+                                 (WIDTH // 3, HEIGHT // 6 * 4, WIDTH // 3, HEIGHT // 6), 0)
+            pygame.display.flip()
+            clock.tick(FPS)
+
+    def terminate(self):
+        pygame.quit()
+        sys.exit()
+
 
 all_sprites = pygame.sprite.Group()
 obstacles = pygame.sprite.Group()
@@ -103,30 +261,78 @@ city_speed = -9
 fence_x = 0
 fence_speed = -13
 
-count_points = 0
 font = pygame.font.Font(None, 36)
 text_color = pygame.Color("red")
+jump = False
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+s = Start()
+Start.start_screen(s)
+
+count_points = 0
 jump = False
 running = True
 count = 0
 count_ramp_hits = 0
+last_obstacle = 0
+last_cone = 0
+last_ramp = 0
+last_up = 0
+last_mid = 0
+last_down = 0
+on_obstacle = False
+
+lanes = {500: last_up, 700: last_mid, 900: last_down}
+
+'''sprites_names = {
+    0: Ramp(lane_y),
+    1: Obstacle(lane_y - 50),
+    2: Cone(lane_y - 50)
+}'''
 
 while running:
     clock.tick(FPS)
+
+    last_obstacle += 1
+    last_cone += 1
+    last_ramp += 1
+    last_up += 1
+    last_mid += 1
+    last_down += 1
+
+
+    if random.randrange(100) < 3:
+        random_obj = random.randint(0, 2)
+        lane_y = random.choice(lanes_y)
+        if random_obj == 1:
+            obstacle = Obstacle(lane_y - 50)
+            all_sprites.add(obstacle)
+            obstacles.add(obstacle)
+            last_obstacle = 0
+        elif random_obj == 2:
+            cone = Cone(lane_y - 50)
+            all_sprites.add(cone)
+            cones.add(cone)
+            last_cone = 0
+        else:
+            ramp = Ramp(lane_y)
+            all_sprites.add(ramp)
+            ramps.add(ramp)
+            last_ramp = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w and not jump:
                 if player.rect.top > lanes_y[0]:
                     player.rect.y -= 200
-            elif event.key == pygame.K_s:
-                if player.rect.bottom < lanes_y[-1]:
+            elif jump is False and event.key == pygame.K_s:
+                if player.rect.bottom not in range(lanes_y[-2] + 1, lanes_y[-1] + 1):
                     player.rect.y += 200
             elif event.key == pygame.K_c and player.rect.y in [ \
                     x - 50 for x in lanes_y
-            ]:
+            ] and not jump:
                 jump = True
 
     if jump:
@@ -139,28 +345,10 @@ while running:
     count_points += 1
     all_sprites.update()
 
-    if random.randrange(100) < 3:
-        random_obj = random.randint(0, 2)
-        if random_obj == 1:
-            lane_y = random.choice(lanes_y)
-            obstacle = Obstacle(lane_y - 50)
-            all_sprites.add(obstacle)
-            obstacles.add(obstacle)
-        elif random_obj == 2:
-            lane_y = random.choice(lanes_y)
-            cone = Cone(lane_y - 50)
-            all_sprites.add(cone)
-            cones.add(cone)
-        else:
-            lane_y = random.choice(lanes_y)
-            ramp = Ramp(lane_y)
-            all_sprites.add(ramp)
-            ramps.add(ramp)
-
     background_x += background_speed
     city_x += city_speed
     fence_x += fence_speed
-    ramp_hits = pygame.sprite.spritecollide(player, ramps, False)
+    ramp_hits = pygame.sprite.spritecollide(player, ramps, False, pygame.sprite.collide_mask)
 
     if ramp_hits:
         count_ramp_hits += 1
@@ -176,10 +364,30 @@ while running:
         End.end_screen(e)
         running = False
 
+    if on_obstacle:
+        player.rect.bottom = obstacle_top + 1
+        print(player.rect.left, obstacle_hits.rect.right)
+        if obstacle_hits.rect.right == 625:
+            on_obstacle = False
+
+
+    obstacles_hits = pygame.sprite.spritecollide(player, obstacles, False, pygame.sprite.collide_mask)
+    if not on_obstacle and obstacles_hits and player.rect.bottom not in range(obstacles_hits[0].rect.top + 1, obstacles_hits[0].rect.bottom):
+        print(player.rect.bottom, range(obstacles_hits[0].rect.top + 1, obstacles_hits[0].rect.bottom))
+        e = End()
+        End.end_screen(e)
+        running = False
+    elif obstacles_hits:
+        print(player.rect.bottom, range(obstacles_hits[0].rect.top + 2, obstacles_hits[0].rect.bottom))
+        obstacle_hits = obstacles_hits[0]
+        obstacle_top = obstacle_hits.rect.top
+        player.rect.bottom = obstacle_top
+
+        on_obstacle = True
+
     screen.fill((0, 0, 0))
     if background_x <= -1300:
         background_x = 0
-
     if city_x <= -1300:
         city_x = 0
 
@@ -196,6 +404,5 @@ while running:
     clock.tick(FPS)
     all_sprites.draw(screen)
     pygame.display.flip()
-
 
 pygame.quit()
